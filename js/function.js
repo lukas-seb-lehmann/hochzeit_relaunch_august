81d72c4856858f24f8afd3a645b5c28179bf1d96
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2022-08-31 14:52:41 +0200

    pixel

--- js/function.js
+++ js/function.js
@@ -46,17 +46,13 @@ function starttracking(){
   gtag('config', 'G-SRC8XTZ052');
 
 	
-	document.getElementById("google-maps-div").innerHTML ='<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d84853.84459905875!2d7.754466!3d48.347405!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x90620af1a9d4fe36!2sLukas%20Lehmann%20-%20Hochzeitsfotograf%20Lahr%2FSchwarzwald!5e0!3m2!1sde!2sde!4v1645819832012!5m2!1sde!2sde" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
 	
 
 
-	
 	
 }
 
-function startpixel(){
-	document.getElementById("meta-pixel").type='text/JavaScript';
-}
+
 
 
 
@@ -87,7 +83,7 @@ function funzustimmen(){
 	containerelement.classList.remove("active");
 	
 	starttracking();
-/*fbq('consent', 'grant');*/
+
 	
 }
 
